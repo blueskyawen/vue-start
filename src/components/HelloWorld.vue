464f72500c2b360a8fbea97227c15cc960b8700b
blueskyawen <bluesky.liu@163.com>
2019-06-04 01:50:47 +0800

    sdsd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,84 +1,29 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="form-group">
+      <label>{{hello.label}}</label>
+      <div class="input-item">{{hello.text}}</div>
+    </div>
+    <div class="form-group">
+      <label>{{htmlMsg.label}}</label>
+      <div class="input-item"><span v-html="htmlMsg.text"></span></div>
+    </div>
+    <div class="form-group">
+      <label>{{filterMsg.label}}</label>
+      <div class="input-item" v-bind:class="{'active':active}">{{ filterMsg.text | capitalize1 | capitalize2}}</div>
+      <button v-on:click="changeClass()">改变样式</button>
+    </div>
+    <div class="form-group">
+      <label>{{ifshow.label}}</label>
+      <div class="input-item" v-show="ifshow.flag">
+        <input type="number" v-model="ifshow.num" />
+      </div>
+      <button @click="hideMag()">{{ifshow.title}}</button>
+    </div>
+    <div class="form-group" v-if="ifshow.num > 1">
+      <label></label>
+      <div class="input-item">{{'我是隐藏hide的元素' + '!!!'}}</div>
+    </div>
   </div>
 </template>
 
@@ -87,7 +32,37 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      hello: {label: '欢迎语', text: 'Welcome to Vue.js'},
+      htmlMsg: {label: 'Html插值', text: '<span style="color:blue">菜鸟教程</span>'},
+      filterMsg: {label: '过滤器', text: 'Welcome'},
+      active: false,
+      ifshow: {label: '数字', flag: true, num: 5, title: '隐藏输入'},
+      masValue: '请输入'
+    }
+  },
+  methods: {
+    changeClass: function () {
+      this.active = !this.active
+    },
+    hideMag: function () {
+      this.ifshow.flag = !this.ifshow.flag
+      if (this.ifshow.flag) {
+        this.ifshow.title = '隐藏输入'
+      } else {
+        this.ifshow.title = '显示输入'
+      }
+    }
+  },
+  filters: {
+    capitalize1: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.toUpperCase() + '111'
+    },
+    capitalize2: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.split('').reverse().join('')
     }
   }
 }
@@ -95,18 +70,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.form-group {
+  width: 420px;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  font-size: 16px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.form-group label {
+  width: 25%;
+  margin-right: 20px;
+  color: #666;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.form-group label:after {
+  content: ":";
 }
-a {
-  color: #42b983;
+.form-group .input-item {
+  width: 75%;
+  color: #666;
+  height: 32px;
+  line-height: 32px;
+}
+.active {
+  color: red !important;
+  font-size: 18px;
+  font-weight: 600;
 }
 </style>
