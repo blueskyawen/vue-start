37011f0192de4fd61af31788643cdd0a0d607544
blueskyawen <bluesky.liu@163.com>
2019-06-22 01:27:14 +0800

    add slot

--- src/components/basic/basic-axios.vue
+++ src/components/basic/basic-axios.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="basicAxios">
-    <div class="form-group">
-      <label>axios-get</label>
-      <div class="input-item">
+    <div class="demo-item-group">
+      <label>1. axios-get</label>
+      <div class="demo-item">
         <button @click.stop="getHttp()">Get</button>
       </div>
+      <p>{{rspInfo}}</p>
+      <p style="color: red">{{errInfo}}</p>
     </div>
-    <p>{{rspInfo}}</p>
-    <p style="color: red">{{errInfo}}</p>
+    <div class="demo-item-group">
+      <label>2. axios-异步加载组件</label>
+      <div class="demo-item">
+        <async-webpack-example></async-webpack-example>
+      </div>
+      <div class="demo-item">
+        <my-component></my-component>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import basicAnimate from './basic-animate'
+import basicExtend from './basic-extend'
+
+Vue.component(
+  'async-webpack-example',
+  () => import('./basic-setter')
+)
+const AsyncComponent = () => ({
+  // 需要加载的组件 (应该是一个 `Promise` 对象)
+  component: import('./basic-setter'),
+  // 异步组件加载时使用的组件
+  loading: basicAnimate,
+  // 加载失败时使用的组件
+  error: basicExtend,
+  // 展示加载时组件的延时时间。默认值是 200 (毫秒)
+  delay: 3000,
+  // 如果提供了超时时间且组件加载也超时了，
+  // 则使用加载失败时使用的组件。默认值是：`Infinity`
+  timeout: 10000
+})
+
 export default {
   name: 'basicAxios',
   data () {
@@ -19,6 +50,9 @@ export default {
       rspInfo: {},
       errInfo: {}
     }
+  },
+  components: {
+    'my-component': AsyncComponent
   },
   methods: {
     getHttp: function () {
@@ -45,6 +79,28 @@ export default {
 </script>
 
 <style scoped>
+  .demo-item-group {
+    display: flex;
+    margin: 10px 30px;
+    flex-direction: column;
+    border-bottom: solid 1px #00cc99;
+  }
+  .demo-item-group > label {
+    font-size: 22px;
+    color: #000080;
+    margin-bottom: 5px;
+    font-weight: 600;
+  }
+  .demo-item-group > label:after {
+    content: ":";
+  }
+  .demo-item-group > .demo-item {
+    width: inherit;
+    margin-bottom: 5px;
+  }
+  .demo-item-group > .demo-item:not(:last-of-type) {
+    border-bottom: dashed 1px #999;
+  }
   .form-group {
     width: 600px;
     margin-bottom: 20px;
@@ -77,12 +133,13 @@ export default {
     cursor: pointer;
     margin-right: 10px;
   }
-  .form-group .input-item > button {
+  button {
     background: #3cc;
     padding: 10px 30px;
     border: none;
     border-radius: 4px;
     font-size: 16px;
     cursor: pointer;
+    outline: none;
   }
 </style>
--- src/components/basic/basic-slot.vue
+++ src/components/basic/basic-slot.vue
@@ -19,7 +19,7 @@
           <template v-slot:slot1>
             <p>I am 内容 in slot1</p>
           </template>
-          <p>I am 内容1 in default slot</p>
+          <p>2.6+最新实现方式</p>
           <p>I am 内容2 in default slot</p>
           <template v-slot:slot2>
             <p>I am 内容 in slot2</p>
@@ -32,7 +32,7 @@
             <p>I am 内容 in slot1</p>
           </template>
           <template slot="default">
-            <p>I am 内容1 in default slot</p>
+            <p>老的实现方式</p>
             <p>I am 内容2 in default slot</p>
           </template>
           <template slot="slot2">
@@ -46,7 +46,7 @@
             <p>I am 内容 in slot1</p>
           </template>
           <template #default>
-            <p>I am 内容1 in default slot</p>
+            <p>新方式的简写版</p>
             <p>I am 内容2 in default slot</p>
           </template>
           <template #slot2>
@@ -68,6 +68,57 @@
             <p>I am 内容 in slot2</p>
           </template>
         </local-compt-2>-->
+      </div>
+    </div>
+    <div class="demo-item-group">
+      <label>4. 作用域插槽</label>
+      <p>slot的实现实际上内部是函数调用function(option) {return ...组件待投影的内容}, 其中option即为可选的插槽prop, 加
+        入在组件内slot绑定的是v-bind：aaa="value"，则option={aaa}，也就是template上得到的变量，比如v-slot:slot1="slotProp"中的slotProp</p>
+      <div class="demo-item">
+        <local-compt3>
+          <template v-slot:slot1="slotProp">
+            <p>{{title}}</p>
+            <p>slot1: {{slotProp.msg}}, slotProp={prop}</p>
+          </template>
+          <template #default="{aha: Aha}">
+            <p>2.6+新实现方式</p>
+            <p>default: {{Aha}}, 结构变量重命名</p>
+          </template>
+          <template #slot2="{slotColor}">
+            <p>slot2: 内容颜色, solt1: {{slotColor.color1}}; default: {{slotColor.color2}}; slot2: {{slotColor.color3}}</p>
+          </template>
+        </local-compt3>
+      </div>
+      <div class="demo-item">
+        <local-compt3>
+          <template slot="slot1" slot-scope="slotProp">
+            <p>{{title}}</p>
+            <p>slot1: {{slotProp.msg}}, slotProp={prop}</p>
+          </template>
+          <template slot-scope="{aha: Aha}">
+            <p>老实现方式</p>
+            <p>default: {{Aha}}, 结构变量重命名</p>
+          </template>
+          <template slot="slot2" slot-scope="{slotColor}">
+            <p>slot2: 内容颜色, solt1: {{slotColor.color1}}; default: {{slotColor.color2}}; slot2: {{slotColor.color3}}</p>
+          </template>
+        </local-compt3>
+      </div>
+    </div>
+    <div class="demo-item-group">
+      <label>5. 独占默认插槽</label>
+      <div class="demo-item">
+        <local-compt3>
+          <template #default="{aha: Aha}">
+            <p>default: {{Aha}}, 结构变量重命名</p>
+          </template>
+        </local-compt3>
+        <local-compt3 v-slot:default="{aha: Aha}">
+          <p>default: {{Aha}}, 结构变量重命名</p>
+        </local-compt3>
+        <local-compt3 v-slot="{aha: Aha}">
+          <p>default: {{Aha}}, 结构变量重命名</p>
+        </local-compt3>
       </div>
     </div>
   </div>
@@ -106,16 +157,38 @@ var localCompt2 = {
              <div :style="{color: styleObj.color3}"><slot name="slot2"></slot></div>
             </div>`
 }
+var localCompt3 = {
+  data () {
+    return {
+      name: '局部组件3',
+      info: {name: 'Jack', age: 18},
+      message: 'i am local component3',
+      aha: 'ahaha',
+      styleObj: {
+        color1: '#ff9933',
+        color2: '#00e64d',
+        color3: '#0059b3'
+      }
+    }
+  },
+  template: `<div><h3>{{name}}</h3>
+             <div :style="{color: styleObj.color1}"><slot name="slot1" v-bind:msg="message"></slot></div>
+             <div :style="{color: styleObj.color2}"><slot v-bind:aha="aha"></slot></div>
+             <div :style="{color: styleObj.color3}"><slot name="slot2" v-bind:slotColor="styleObj"></slot></div>
+            </div>`
+}
+
 export default {
   name: 'basic-slot',
   data: function () {
     return {
       message: 'hello slot',
       slot1: 'slot1',
-      slot2: 'slot2'
+      slot2: 'slot2',
+      title: 'hello 作用域插槽 !'
     }
   },
-  components: {localCompt1, localCompt2}
+  components: {localCompt1, localCompt2, localCompt3}
 }
 </script>
 
