acfb1d7395a9db8ad0afb70a729f3a448e1d5b70
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2019-08-02 17:47:34 +0800

    add refs use

--- src/components/basic/basic-setter.vue
+++ src/components/basic/basic-setter.vue
@@ -5,18 +5,36 @@
     <div class="demo-item-group">
       <label>1. 父实例和子组件引用</label>
       <div class="demo-item">
-        <localCompt5 ref="RelCompt5"></localCompt5>
+        <localCompt5 :ref="refCmpt5" inline-template>
+          <div>
+            <p>These are compiled as the component's own template.</p>
+            <p>Not parent's transclusion content.</p>
+          </div>
+        </localCompt5>
       </div>
       <div class="demo-item">
         <localCompt6 ref="RelCompt6"></localCompt6>
       </div>
+      <p>存在多个同名Ref的时候, $refs.RefName获取到的是最后的那个</p>
     </div>
     <div class="demo-item-group">
-      <label>2. 依赖注入：只可用于属性的注入获取，不允许修改和注入方法, 只用于祖先级逐渐的属性共享，用处有限</label>
+      <label>2. Ref和v-for </label>
+      <div class="demo-item" v-for="(value, index) in tempsss" :key="value+index">
+        <localCompt5 :ref="value" :show-op="false"></localCompt5>
+      </div>
+      <div class="demo-item">
+        <button @click="modCount(0)">Oper-ref0</button>
+        <button @click="modCount(1)">Oper-ref1</button>
+        <button @click="modCount(2)">Oper-ref2</button>
+      </div>
+      <p>$refs.RefNames包含对应数据源的这些子组件的数组</p>
+    </div>
+    <div class="demo-item-group">
+      <label>3. 依赖注入：只可用于属性的注入获取，不允许修改和注入方法, 只用于祖先级逐渐的属性共享，用处有限</label>
       <p>provide不能注入变量或方法或计算属性，只能注入常量或新建对象，如：new Date()</p>
       <p>而且注入的属性是非响应的，不能直接用于模版语法里</p>
       <div class="demo-item">
-        <localCompt6 ref="RelCompt6"></localCompt6>
+        <localCompt6 :ref="'RelCompt6'"></localCompt6>
       </div>
       <div class="demo-item">
         <localCompt7></localCompt7>
@@ -29,6 +47,12 @@
 import Vue from 'vue'
 
 Vue.component('localCompt5', {
+  props: {
+    showOp: {
+      type: Boolean,
+      default: true
+    }
+  },
   data: function () {
     return {
       name: 'localCompt5',
@@ -36,8 +60,8 @@ Vue.component('localCompt5', {
       injectMsg: 'Inject msg'
     }
   },
-  template: `<div><span>{{name}} count: {{count}}</span><button @click="addCount">add count</button>
-<button @click="modParent">mod perant and childs</button></div>`,
+  template: `<div><span>{{name}} count: {{count}}</span><button v-if="showOp" @click="addCount">add count</button>
+<button v-if="showOp" @click="modParent">mod perant and childs</button></div>`,
   methods: {
     addCount: function () {
       this.count += 2
@@ -85,14 +109,19 @@ export default {
     return {
       name: 'basicSetter',
       message: 'hello setter !',
-      injectMsg: 'Inject basicSetter'
+      injectMsg: 'Inject basicSetter',
+      tempsss: ['temps', 'temps', 'temps'],
+      refCmpt5: 'RelCompt5'
     }
   },
   components: {localCompt6, localCompt7},
   methods: {
     modChildCompName: function () {
-      this.$refs.RelCompt5.name += ' hello'
+      this.$refs['RelCompt5'].name += ' hello'
       this.$refs.RelCompt6.name += ' hello'
+    },
+    modCount: function (index) {
+      this.$refs.temps[index].addCount()
     }
   },
   provide: {
