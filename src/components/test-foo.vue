bcc7f0ba259f543e77b4e68415396660dec133e9
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2020-01-08 15:16:35 +0800

    ad test unil

---
+++ src/components/test-foo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="message">{{ message }}</div>
+    Enter your username: <input v-model="username">
+    <div v-if="error" class="error">
+      Please enter a username with at least seven letters.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test-Foo',
+  data () {
+    return {
+      message: 'Welcome to the Vue Start',
+      username: ''
+    }
+  },
+  computed: {
+    error () {
+      return this.username.trim().length < 7
+    }
+  }
+}
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
