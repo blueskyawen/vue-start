d1d4e214befdaa9b230b475e20e05886364105a8
blueskyawen <bluesky.liu@163.com>
2020-01-14 00:15:32 +0800

    Vcg: Update live demo

--- static/js/6.92dc85f497ede9dc8a8e.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([6],{iXqC:function(e,o){e.exports={compilerOptions:{module:"commonjs",target:"es5",sourceMap:!0},exclude:["node_modules"]}}});
-//# sourceMappingURL=6.92dc85f497ede9dc8a8e.js.map
---
+++ static/js/7.7a8f84a5435cef9ef1b5.js
@@ -0,0 +1,2 @@
+webpackJsonp([7],{iXqC:function(e,o){e.exports={compilerOptions:{module:"commonjs",target:"es5",sourceMap:!0},exclude:["node_modules"]}}});
+//# sourceMappingURL=7.7a8f84a5435cef9ef1b5.js.map
