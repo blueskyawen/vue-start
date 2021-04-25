d9af3e15ab37b4ec95c4d41e859d5a0619f6db6e
blueskyawen <bluesky.liu@163.com>
2021-04-26 02:01:11 +0800

    add ckeditor

--- src/main.js
+++ src/main.js
@@ -39,14 +39,12 @@ Vue.use(VueTinymce)
 
 import 'highlight.js/styles/monokai-sublime.css'
 
+import CKEditor from '@ckeditor/ckeditor5-vue2';
+Vue.use( CKEditor );
+
 Vue.use(VueAxios, axios)
 Vue.use(vcCat)
 Vue.use(VcCat)
-
-/* import CKEditor from '@ckeditor/ckeditor5-vue2';
-import '@ckeditor/ckeditor5-build-classic/build/translations/zh-cn';
-// import '@ckeditor/ckeditor5-build-decoupled-document/build/translations/zh-cn';
-Vue.use(CKEditor); */
 
 Vue.config.productionTip = false
 Vue.config.keyCodes.awen = 67
