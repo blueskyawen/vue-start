6441acdd84a239435ca0d605b4d5a8b9032289c8
liuxw34796 <liuxw34796@hundsun.com>
2021-04-29 14:00:27 +0800

    Vcg: Update live demo

--- static/js/10.5ddee1db2647f621e5dc.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([10],{"645S":function(n,c){}});
-//# sourceMappingURL=10.5ddee1db2647f621e5dc.js.map
--- static/js/11.527af9e8fd273db719f5.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([11],{"Q0/0":function(n,c){}});
-//# sourceMappingURL=11.527af9e8fd273db719f5.js.map
