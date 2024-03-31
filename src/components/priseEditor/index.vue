de97e7f8ed200af63c76113b42413d9749ff0fe8
blueskyawen <bluesky.liu@163.com>
2024-04-01 02:19:03 +0800

    feat: aabb

--- src/components/priseEditor/index.vue
+++ src/components/priseEditor/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="prise-editor">
     <div class="prise-cfg">
-      <vc-button @click="addOneline">新增一行产品</vc-button>
+      <vc-button type="add" @click="addOneline">新增一行产品</vc-button>
+      <vc-button @click="saveList" style="margin-left: 10px">保存</vc-button>
+      <vc-button type="cancel" @click="clear" style="margin-left: 10px"
+        >清空</vc-button
+      >
     </div>
     <div class="prise-list">
       <div class="col-head">
@@ -10,14 +14,14 @@
         >
       </div>
       <div class="col-body" v-if="list.length">
-        <div class="col-line" v-for="(item, index) in list" :key="index">
+        <div class="col-line" v-for="item in list" :key="item.lineindex">
           <span class="col-item" v-for="col2 in columns" :key="col2.key">
             <vc-button
               v-if="col2.key == 'oper'"
               size="custom"
               height="24px"
               style="margin-top: 6px;"
-              @click="delOneline(index)"
+              @click="delOneline(item)"
               >删除</vc-button
             >
             <vc-input
@@ -46,6 +50,10 @@ export default {
         {
           label: "拿货价",
           key: "oldPrice"
+        },
+        {
+          label: "运费",
+          key: "yunfei"
         },
         {
           label: "标价倍数",
@@ -87,15 +95,27 @@ export default {
       list: []
     };
   },
+  created() {
+    let coachData = localStorage.getItem("doudianPriceList");
+    if (coachData) {
+      this.list = JSON.parse(coachData);
+    }
+  },
   methods: {
-    delOneline: function(index) {
-      this.list.splice(index, 1);
+    delOneline: function(item) {
+      if (!this.list || !this.list.length) return;
+      let delIndex = this.list.findIndex(x => x.lineindex == item.lineindex);
+      if (delIndex > -1) {
+        this.list.splice(delIndex, 1);
+      }
     },
     addOneline: function() {
       this.list.push({
+        lineindex: new Date().valueOf(),
         name: "",
         oldPrice: 0,
         saleB: 1,
+        yunfei: 0,
         newPrice: 0,
         zhekou: 0.8,
         youhuiquan: 3,
@@ -109,7 +129,17 @@ export default {
     inputChange: function(line, key) {
       let value = +line[key];
       if (key !== "name") {
-        line.newPrice = (+line.oldPrice * +line.saleB).toFixed(2);
+        if (["newPrice", "saleB", "oldPrice", "yunfei"].includes(key)) {
+          if (key == "newPrice") {
+            line.newPrice = value.toFixed(2);
+          } else {
+            line.newPrice = (
+              +line.oldPrice * +line.saleB +
+              +line.yunfei
+            ).toFixed(2);
+          }
+        }
+
         line.shijiprice = (
           +line.newPrice * +line.zhekou -
           line.youhuiquan
@@ -117,10 +147,22 @@ export default {
         line.lirun = (
           +line.shijiprice -
           +line.oldPrice -
+          +line.yunfei -
           +line.shijiprice * +line.pingtaikoudian
         ).toFixed(2);
         line.lirunlv =
           ((+line.lirun * 100) / +line.shijiprice).toFixed(2) + "%";
+      }
+    },
+    saveList: function() {
+      let coachData = this.list && this.list.length ? this.list : [];
+      localStorage.setItem("doudianPriceList", JSON.stringify(coachData));
+    },
+    clear() {
+      this.list = [];
+      let coachDataItem = localStorage.getItem("doudianPriceList");
+      if (coachDataItem) {
+        localStorage.setItem("doudianPriceList", JSON.stringify([]));
       }
     }
   }
